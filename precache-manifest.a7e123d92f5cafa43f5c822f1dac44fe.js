c0176bde9b26155aa642363c43233b0406ec919b
Decio Soares <decio.msoares@gmail.com>
2021-04-21 16:50:55 +0100

    Updates

---
+++ precache-manifest.a7e123d92f5cafa43f5c822f1dac44fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bbc955475d6958487abe988a7cecf0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bee8133ee3981d081dc6",
+    "url": "/portfolio/static/css/main.1f87b4ae.chunk.css"
+  },
+  {
+    "revision": "5902c5697fb1fca5ea1c",
+    "url": "/portfolio/static/js/2.2af72535.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.2af72535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee8133ee3981d081dc6",
+    "url": "/portfolio/static/js/main.c09ed0bb.chunk.js"
+  },
+  {
+    "revision": "6707b002e393967504a9",
+    "url": "/portfolio/static/js/runtime-main.d6a91bb8.js"
+  },
+  {
+    "revision": "5e343b09fc81c99ac53895b6af1ed02d",
+    "url": "/portfolio/static/media/manifest.5e343b09.md"
+  },
+  {
+    "revision": "25e9f2736f9bb13fff5e65ba0acae093",
+    "url": "/portfolio/static/media/profile_red_02_small.25e9f273.png"
+  }
+]);
